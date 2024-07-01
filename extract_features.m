91c68c628e7ae16ad690682522a5269c97a0184f
SasheO <76496417+SasheO@users.noreply.github.com>
2024-07-01 12:55:42 -0400

    more work on extracting features

--- extract_features.m
+++ extract_features.m
@@ -57,4 +57,65 @@ procs = Append_process(procs, 'FFT_filterbank_proc');
 procs = Append_process(procs, 'Power_sum_envelope_proc');
 procs = Append_process(procs, 'Gain_proc');
 
-processed_sound5 = Process(procs, wav_file_sound);
+processed_sound = Process(procs, wav_file_sound(12248:14891));
+writematrix(processed_sound, "processed_sound.csv")
+
+
+%% feature extraction: 
+		% extract features into similar directory with good naming convention.
+		% 	look at the directory structure for a minute test. test the creation of folders.
+		% 	plan how you will separate phonemes (maybe folders?)
+		% 	run code on phn files to extract phoneme features.
+
+source = "Y:/personal/chu.kevin/Sentences/TIMIT_dp/DEV/DR1/FAKS0"; % should start with Y:/
+destination = "Y:/personal/ojuba.mezisashe"; % should start with Y:/
+
+folders_source = strsplit(source, "/"); % the first three are what is switched out with destination.
+
+for n = 4 : length(folders_source)
+    mkdir(destination,folders_source(n))
+    destination = destination + "/" + folders_source(n);
+end
+
+files = dir(fullfile(source, '*.PHN'));
+
+for file_indx = 1:length(files)
+    
+    file_name = files(file_indx);
+
+    if file_name.name(1)=="."
+        continue % skip all the "._ ..." files
+    end
+
+    phn_file = file_name.folder + "/" + file_name.name;
+    wav_file = file_name.folder + "/" + file_name.name(1:end-3)+"WAV";
+
+    opts = detectImportOptions(phn_file, "FileType","delimitedtext");
+    opts.ExtraColumnsRule = 'ignore';
+    disp(opts)
+    opts = setvartype(opts,{'Var1','Var2','Var3',},{'int32','int32','string'});
+    times_to_phoneme = table2array(readtable(phn_file,opts));
+
+    [wav_file_sound,Fs] = audioread(wav_file);
+    
+    
+    
+    % for n = 1 : height(times_to_phoneme)
+    %     phoneme_start_indx = str2double(times_to_phoneme(n,1))+1;
+    %     phoneme_end_indx = str2double(times_to_phoneme(n,2))+1;
+    %     phoneme_label = times_to_phoneme(n,3);
+    %     processed_sound = wav_file_sound(phoneme_start_indx:phoneme_end_indx);
+    %     mkdir(destination,phoneme_label);
+    % 
+    %     while isfile(filename)
+    %         statements
+    %     end
+    % end
+end
+
+
+% if isfile(filename)
+%      % File exists.
+% else
+%      % File does not exist.
+% end
