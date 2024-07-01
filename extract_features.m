e4828dead21e746921909e19e14fcefc0370aa59
SasheO <76496417+SasheO@users.noreply.github.com>
2024-07-01 13:13:17 -0400

    completed feature extraction according to phonemes

--- extract_features.m
+++ extract_features.m
@@ -1,53 +1,8 @@
-%% check file directory listing all relevant files there.
-
-file_directory = "Y:\personal\chu.kevin\Sentences\TIMIT_dp\DEV\DR1\FAKS0";
-files = dir(fullfile(file_directory, '*.PHN'));
-
-for n = 1 : length(files)
-    file_name = files(n);
-
-    % do processing here
-end
-
-%% test to extract feature from one file
-file_name = "Y:\personal\chu.kevin\Sentences\TIMIT_dp\DEV\DR1\FAKS0\SI2203.";
-
-phn_file = file_name+"PHN";
-wav_file = file_name+"WAV";
-
-
-opts = detectImportOptions(phn_file, "FileType","delimitedtext");
-opts.ExtraColumnsRule = 'ignore';
-disp(opts)
-opts = setvartype(opts,{'Var1','Var2','Var3',},{'int32','int32','string'});
-times_to_phoneme = table2array(readtable(phn_file,opts));
-
-%% 		test opening one file with a single phoneme. 
-% do for loop for all phonemes.
-
-bits = 16;
-file_name = "Y:\personal\chu.kevin\Sentences\TIMIT_dp\DEV\DR1\FAKS0\SI2203.WAV";
-[wav_file_sound,Fs] = audioread(file_name);
-
-for n = 1 : height(times_to_phoneme)
-    phoneme_start_indx = str2double(times_to_phoneme(n,1))+1;
-    phoneme_end_indx = str2double(times_to_phoneme(n,2))+1;
-    phoneme_label = times_to_phoneme(n,3);
-    sound(wav_file_sound(phoneme_start_indx:phoneme_end_indx),Fs,bits);
-    pause(0.3);
-end
-
-%% feature extraction: (sample here https://www.mathworks.com/matlabcentral/fileexchange/159688-nucleus-toolbox)
-
-% file_name = "Y:\personal\chu.kevin\Sentences\TIMIT_dp\DEV\DR1\FAKS0\SI2203.WAV";
-% procs = ACE_map;
-% processed_sound = Process(procs, wav_file_sound);
-% Plot_sequence(processed_sound);
-
-bits = 16;
-file_name = "Y:\personal\chu.kevin\Sentences\TIMIT_dp\DEV\DR1\FAKS0\SI2203.WAV";
-[wav_file_sound,Fs] = audioread(file_name);
-
+%% feature extraction: 
+		% extract features into similar directory with good naming convention.
+		% 	look at the directory structure for a minute test. test the creation of folders.
+		% 	plan how you will separate phonemes (maybe folders?)
+		% 	run code on phn files to extract phoneme features.
 procs = [];
 procs = Ensure_field(procs, 'map_name', 'EXTRACT_FEATURES');
 
@@ -56,16 +11,6 @@ procs = Append_front_end_processes(procs);
 procs = Append_process(procs, 'FFT_filterbank_proc');
 procs = Append_process(procs, 'Power_sum_envelope_proc');
 procs = Append_process(procs, 'Gain_proc');
-
-processed_sound = Process(procs, wav_file_sound(12248:14891));
-writematrix(processed_sound, "processed_sound.csv")
-
-
-%% feature extraction: 
-		% extract features into similar directory with good naming convention.
-		% 	look at the directory structure for a minute test. test the creation of folders.
-		% 	plan how you will separate phonemes (maybe folders?)
-		% 	run code on phn files to extract phoneme features.
 
 source = "Y:/personal/chu.kevin/Sentences/TIMIT_dp/DEV/DR1/FAKS0"; % should start with Y:/
 destination = "Y:/personal/ojuba.mezisashe"; % should start with Y:/
@@ -99,23 +44,19 @@ for file_indx = 1:length(files)
     [wav_file_sound,Fs] = audioread(wav_file);
     
     
-    
-    % for n = 1 : height(times_to_phoneme)
-    %     phoneme_start_indx = str2double(times_to_phoneme(n,1))+1;
-    %     phoneme_end_indx = str2double(times_to_phoneme(n,2))+1;
-    %     phoneme_label = times_to_phoneme(n,3);
-    %     processed_sound = wav_file_sound(phoneme_start_indx:phoneme_end_indx);
-    %     mkdir(destination,phoneme_label);
-    % 
-    %     while isfile(filename)
-    %         statements
-    %     end
-    % end
+    for n = 1 : height(times_to_phoneme)
+        phoneme_start_indx = str2double(times_to_phoneme(n,1))+1;
+        phoneme_end_indx = str2double(times_to_phoneme(n,2))+1;
+        phoneme_label = times_to_phoneme(n,3);
+        processed_sound = Process(procs, wav_file_sound(phoneme_start_indx:phoneme_end_indx));
+        mkdir(destination,phoneme_label);
+        
+        file_num = 1;
+        while isfile(destination + "/"+ phoneme_label + "/" + file_name.name(1:end-4)+ "-"+ num2str(file_num) + ".csv")
+            file_num = file_num +1;
+        end
+        writematrix(processed_sound, destination + "/"+ phoneme_label + "/" + file_name.name(1:end-4)+ "-"+ num2str(file_num) + ".csv")
+    end
+
 end
 
-
-% if isfile(filename)
-%      % File exists.
-% else
-%      % File does not exist.
-% end
